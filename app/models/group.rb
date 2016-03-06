0ae388b285d2e11b0a8c020333fea1309a57a3ca
Jessica Jones <jjones@demacmedia.com>
2016-03-06 13:17:54 -0500

    Restart Rails App

--- app/models/group.rb
+++
@@ -1,5 +0,0 @@
-class Group < ActiveRecord::Base
-
-has_many :comments
-
-end
--- db/migrate/20160227205520_remove_incept_date_from_group.rb
+++
@@ -1,5 +0,0 @@
-class RemoveInceptDateFromGroup < ActiveRecord::Migration
-  def change
-    remove_column :groups, :inceptDate, :string
-  end
-end

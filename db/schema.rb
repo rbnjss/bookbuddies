6f31390b796ae3dec286e697145b7034758683b3
Jessica Jones <jjones@demacmedia.com>
2016-03-10 17:51:05 -0500

    Add first name, last name, and city columns to user. Set up registrations controller for devise

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160308014637) do
+ActiveRecord::Schema.define(version: 20160310224656) do
 
   create_table "books", force: :cascade do |t|
     t.string   "book_name"
@@ -78,6 +78,9 @@ ActiveRecord::Schema.define(version: 20160308014637) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "city"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

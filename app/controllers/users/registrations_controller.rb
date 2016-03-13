7f7cbba3354856d5c067937f48c2bc5fa158585b
Jessica Jones <jjones@demacmedia.com>
2016-03-13 15:03:11 -0400

    Remove profile fields from User

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -61,10 +61,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   private
 
     def sign_up_params
-      params.require(:user).permit(:first_name, :last_name, :city, :email, :password, :password_confirmation)
+      params.require(:user).permit(:email, :password, :password_confirmation)
     end
 
-    def account_update_params
-      params.require(:user).permit(:first_name, :last_name, :city, :email, :password, :password_confirmation, :current_password)
-    end
+    # def account_update_params
+    #   params.require(:user).permit(:email, :password, :password_confirmation, :current_password)
+    # end
 end

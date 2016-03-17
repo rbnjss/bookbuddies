c925a008be325ebe67256b8452b8205028be1aa0
Robin Hamill <hamillrobin@gmail.com>
2016-03-17 18:07:02 -0400

    Fixededit and update actions in books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
 before_action :set_book, only: [:show, :edit, :update, :destroy]
-before_action :set_group, only: [:edit, :new, :update]
+before_action :set_group, only: [:edit, :new]
 
 
 	def index

82e780cb77966bb634958ca4d7268082e57b7807
Jessica Jones <jjones@demacmedia.com>
2016-03-21 19:48:14 -0400

    Add file uploads to profile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313175353) do
+ActiveRecord::Schema.define(version: 20160321233813) do
 
   create_table "books", force: :cascade do |t|
     t.string   "book_name"
@@ -58,9 +58,13 @@ ActiveRecord::Schema.define(version: 20160313175353) do
     t.string   "last_name"
     t.string   "city"
     t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
     t.integer  "user_id"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"

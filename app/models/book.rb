90bf9fedcfe408531fa47fd93aa0ec691c82a2f8
Robin Hamill <hi@robinhamill.ca>
2016-03-25 21:46:11 -0400

    just trying things. no progress

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,7 +3,7 @@ class Book < ActiveRecord::Base
   validates_attachment_content_type :cover, :content_type => /\Aimage\/.*\Z/
 
 	belongs_to :group
-	has_many :comments
+	has_many :comments, :dependent => :destroy
 
 	has_attached_file :cover, styles: { medium: "300x300>", thumb: "100x100>" }
 	validates_attachment_content_type :cover, :content_type => /\Aimage\/.*\Z/

c07001ee7654b54b8271a9a93906771827aeae35
Robin Hamill <hamillrobin@gmail.com>
2016-03-27 14:41:45 -0400

    setting up InviteMailer

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,5 +1,9 @@
 class InviteMailer < ApplicationMailer
+	default from: "hamillrobin@gmail.com"
 
-
+	def book_group_invite
+		mail  to: :email,
+					subject: "Book buddies group invitation"
+	end
 
 end

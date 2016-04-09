321e9789f9737fc78c7b54084d6a0402604b843f
Jessica Jones <jjones@demacmedia.com>
2016-04-08 20:01:37 -0400

    Get comment notifications working

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,2 +1,7 @@
 class NotificationsController < ApplicationController
+	def new
+	end
+
+	def create
+	end
 end
--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,13 +1,13 @@
 class NotificationMailer < ApplicationMailer
+	require 'mailgun'
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.notification_mailer.notification_email.subject
-  #
-  def notification_email
-    @greeting = "Hi"
+	default from: "bookbuddies@sandboxdb186207bc4648cb947c92d270219a9e.mailgun.org"
 
-    mail to: "to@example.org"
-  end
+	def notification_email(user, comment, book_name, book_id)
+		@user = user
+		@comment = comment
+		@book_name = book_name
+		@book = book_id
+		mail to: @user.email, subject: "Bookbuddies: One of your buddies made a new comment!"
+	end
 end

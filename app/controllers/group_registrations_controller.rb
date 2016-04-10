217ed64f728b2fdf8841f09ff5459d2d8e54e996
Robin Hamill <hi@robinhamill.ca>
2016-04-10 11:47:23 -0400

    Leaving a group now destoys any favorites for books within that group. No more orphan links

--- app/controllers/group_registrations_controller.rb
+++ app/controllers/group_registrations_controller.rb
@@ -2,6 +2,12 @@ class GroupRegistrationsController < ApplicationController
   
   def destroy
 		@gr = GroupRegistration.find_by(user_id: params[:user_id], group_id: params[:group_id])
+		@fav = Favorite.where(user_id: current_user.id, book_id: @gr.group.books)
+		if @fav != nil
+			@fav.each do |fav|
+				fav.destroy
+			end
+		end
 		@gr.destroy
 		respond_to do |format|
 		  format.html { redirect_to :groups, notice: 'Registration was successfully destroyed.' }

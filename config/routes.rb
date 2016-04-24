7b9c703e297b55b0f27043775bdc8383e8fdfb95
Jessica Jones <jjones@demacmedia.com>
2016-04-24 18:41:21 -0400

    Configure SSL Encryption for Letsencrypt

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,7 @@ Rails.application.routes.draw do
   end
 
   root to: "pages#home"
+
+  get '/.well-known/acme-challenge/iuk2EFE2Qv9-Ot7MasPLo4Y0sKEdeGkx9UJ9_gica-M.5EioOBuTgSj5ZhG2CkqbPYMf1x8u9DOP3X28H8glwis' => 'pages#letsencrypt'
   
 end
